8ca2617a007a866ea776bd9e1093639756348cf8
Michael Farrell <micolous+git@gmail.com>
2013-01-29 11:12:05 +1100

    major refactor so that we no longer use query arguments, to hopefully fix issues with navigation breaking out into Safari on iOS

--- schedule.js
+++ schedule.js
@@ -17,6 +17,8 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+var schedule = new Array();
+
 function timeFormat(input) {
 	return input.format('HH:mm');
 }
@@ -39,12 +41,148 @@ function parseTimeDelta(input) {
 	return Math.abs(60 * toks[0]) + Math.abs(toks[1]) + Math.abs(toks[2] / 60);
 }
 
+function beginPage() {
+	$.mobile.loading('show', {text: 'Loading the schedule...', textVisible: true});
+	$('#scheduleContainer').empty().trigger('destroy').append(
+		$('<ul>').attr({'data-role': 'listview', 'id': 'scheduleList'}).addClass('ui-listview')
+	);
+}
+
+function endPage() {
+	$('#scheduleContainer').trigger('create');
+	$.mobile.loading('hide');
+}
+
+function displaySchedule(date) {
+	beginPage();
+	
+	var day = moment(date);
+
+	// select a particular date
+	// parse the date
+	schedule.sort(function(a, b){
+		if (a.start < b.start)
+			return -1;
+
+		if (a.start > b.start)
+			return 1;
+
+		// items are at same time, sort by room
+		if (a.room < b.room)
+			return -1;
+
+		if (a.room > b.room)
+			return 1;
+
+		// shouldn't get here, but no big deal
+		return 0;
+	});
+
+	$('#title').text(day.format('dddd Do MMMM YYYY'));
+
+	day = isoDate(day);
+
+	//alert(day);
+	var lastTime = null;
+	$.each(schedule, function(i, e) {
+		if (isoDate(e.start) != day) {
+			// continue
+			return;
+		}
+
+		if (timeFormat(e.start) != lastTime) {
+			lastTime = timeFormat(e.start);
+			$('#scheduleList').append(
+				$('<li>').attr('data-role', 'list-divider').text(timeFormat(e.start))
+			);
+		}
+
+		var endTime = e.start.add('minutes', e.durationMins);
+		var label = e.room + ': ' + e.name + ' ' + (e.presenter ? '- ' + e.presenter : '') + ' (until ' + timeFormat(endTime) + ')';
+		if (e.uri == null) {
+			var item = $('<li>').attr('href', e.uri).text(label);
+		} else {
+			var item = $('<li>').append($('<a>').attr({'href': e.uri, 'rel': 'external'}).text(label));
+		}
+	
+		$('#scheduleList').append(item	);
+	});
+
+	endPage();
+}
+
+function displayAbout() {
+	beginPage();
+	
+	$('#scheduleContainer').empty().append(
+		$('<p>').text(
+			'This is an unofficial service using data parsed from LCA\'s JSON calendar feed.  This information may not be current -- it is designed to be dropped onto Zookeepr later (so UI is all in Javascript).  Please direct feedback to micolous on #linux.conf.au.'
+		)
+	).append(
+		$('<p>').append(
+			$('<a>').attr({
+				'href': 'https://github.com/micolous/lca-mobile-programme',
+				'rel': 'external'
+			}).text('Git repository')
+		)
+	);
+	
+	endPage();
+}
+
+function displayDaySelector() {
+	beginPage();
+	// select date
+	var dates = new Array();
+	$.each(schedule, function(i, e) {
+		// format the date to ISO
+		fdate = isoDate(e.start);
+
+		if (dates.indexOf(fdate) == -1) {
+			// not in list, add
+			dates.push(fdate);
+		}
+
+	});
+
+	dates.sort();
+
+	// now print the list of dates
+	$.each(dates, function(i, e) {
+		$('#scheduleList').append(
+			$('<li>').append(
+				$('<a>')
+					.data('date', e)
+					.bind('click', function(evt){
+						displaySchedule($(this).data('date'));
+					})
+					.text(moment(e).format('dddd, Do MMMM YYYY'))
+			)
+		)
+	});
+
+	$('#scheduleList').append(
+		$('<li>').append(
+			$('<a>')
+				.bind('click', displayAbout)
+				.text('About / Help')
+		)
+	).append(
+		$('<li>').append(
+			$('<img>').attr('src', 'images/qr.png').css('margin', '2px')
+		).append(
+			$('<span>').text('http://lcamp.micolous.id.au/')
+		)
+	);
+	endPage();
+}
+
 $(document).bind("mobileinit", function(){
 	$.mobile.ajaxEnabled = false;
 });
 
 $('#scheduleMain').live('pageinit', function(evt) {
-	$.mobile.loading('show', {text: 'Loading the schedule...', textVisible: true});
+	beginPage();
 	
 	try {
 		var schedule_req = $.ajax('./schedule.json', { async: false });
@@ -55,13 +193,7 @@ $('#scheduleMain').live('pageinit', function(evt) {
 		$('#scheduleContainer').text('Sorry, schedule.json could not load :(');
 		return;
 	}
-	
-	$('#scheduleContainer').empty().trigger('destroy').append(
-		$('<ul>').attr({'data-role': 'listview', 'id': 'scheduleList'}).addClass('ui-listview')
-	);
 
-	
-	var schedule = Array();
 	$.each(schedule_raw, function(i, e) {
 		// reformat the object in to something that is a bit more readable
 		schedule.push({
@@ -77,123 +209,22 @@ $('#scheduleMain').live('pageinit', function(evt) {
 			'durationMins': parseTimeDelta(e.Duration)
 
 		});
-
-
-		//$('#scheduleList').append(
-		//	$('<li>').text(
 	});
 
 
 	// now determine whether to show a date list or the actual info
+	
+	// For campatibility, still handle these query strings.
 	if (location.search.length >= 8) {
-
-		var day = moment(location.search.substr(1, 10));
-
-		// select a particular date
-		// parse the date
-		schedule.sort(function(a, b){
-			if (a.start < b.start)
-				return -1;
-
-			if (a.start > b.start)
-				return 1;
-
-			// items are at same time, sort by room
-			if (a.room < b.room)
-				return -1;
-
-			if (a.room > b.room)
-				return 1;
-
-			// shouldn't get here, but no big deal
-			return 0;
-		});
-
-		$('#title').text(day.format('dddd Do MMMM YYYY'));
-
-		day = isoDate(day);
-
-		//alert(day);
-		var lastTime = null;
-		$.each(schedule, function(i, e) {
-			if (isoDate(e.start) != day) {
-				// continue
-				return;
-			}
-
-			if (timeFormat(e.start) != lastTime) {
-				lastTime = timeFormat(e.start);
-				$('#scheduleList').append(
-					$('<li>').attr('data-role', 'list-divider').text(timeFormat(e.start))
-				);
-			}
-
-			var endTime = e.start.add('minutes', e.durationMins);
-			var label = e.room + ': ' + e.name + ' ' + (e.presenter ? '- ' + e.presenter : '') + ' (until ' + timeFormat(endTime) + ')';
-			if (e.uri == null) {
-				var item = $('<li>').attr('href', e.uri).text(label);
-			} else {
-				var item = $('<li>').append($('<a>').attr({'href': e.uri, 'rel': 'external'}).text(label));
-			}
-		
-			$('#scheduleList').append(item	);
-		});
+		displaySchedule(location.search);
 	} else if (location.search.substr(1, 3) == 'wat') {
-		$('#scheduleContainer').empty().append(
-			$('<p>').text(
-				'This is an unofficial service using data parsed from LCA\'s JSON calendar feed.  This information may not be current -- it is designed to be dropped onto Zookeepr later (so UI is all in Javascript).  Please direct feedback to micolous on #linux.conf.au.'
-			)
-		).append(
-			$('<p>').append(
-				$('<a>').attr({
-					'href': 'https://github.com/micolous/lca-mobile-programme',
-					'rel': 'external'
-				}).text('Git repository')
-			)
-		);
+		displayAbout();
 	} else {
-		// select date
-		var dates = new Array();
-		$.each(schedule, function(i, e) {
-			// format the date to ISO
-			fdate = isoDate(e.start);
-
-			if (dates.indexOf(fdate) == -1) {
-				// not in list, add
-				dates.push(fdate);
-			}
-
-		});
-
-		dates.sort();
-
-		// now print the list of dates
-		$.each(dates, function(i, e) {
-			$('#scheduleList').append(
-				$('<li>').append(
-					$('<a>').attr({'href': '?' + e, 'data-ajax': 'false'}).text(moment(e).format('dddd, Do MMMM YYYY'))
-				)
-			)
-		});
-
-		$('#scheduleList').append(
-			$('<li>').append(
-				$('<a>').attr({'href': '?wat', 'data-ajax': 'false'}).text('About / Help')
-			)
-		).append(
-			$('<li>').append(
-				$('<img>').attr('src', 'images/qr.png').css('margin', '2px')
-			).append(
-				$('<span>').text('http://lcamp.micolous.id.au/')
-			)
-		);
-
+		displayDaySelector();
 	}
 
 
 
-	$('#scheduleContainer').trigger('create');
-	$.mobile.loading('hide');
 
 });
 
